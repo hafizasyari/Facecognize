e25423a4067379c23456cc47a45b8048a2040bee
hafizasyari <hafizasyari@gmail.com>
2017-10-26 20:19:50 +0700

    First Commit

--- app/src/main/java/com/asyariproject/facecognize/MainActivity.java
+++ app/src/main/java/com/asyariproject/facecognize/MainActivity.java
@@ -1,13 +1,77 @@
 package com.asyariproject.facecognize;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView Start, About, Exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Start = (TextView) findViewById(R.id.btnstart);
+        About = (TextView) findViewById(R.id.btnabout);
+        Exit = (TextView) findViewById(R.id.btnexit);
+
+        Start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent Start = new Intent(Menu.this, Start.class);
+                //startActivity(Start);
+            }
+        });
+
+        About.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent About = new Intent(MainActivity.this, About.class);
+                startActivity(About);
+            }
+        });
+
+        Exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                keluar();
+            }
+        });
+
+    }
+
+    public void keluar(){
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+
     }
 }
